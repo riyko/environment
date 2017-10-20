b3d0a8e1d28e6131ad065ee983ca4458ceb0d7f6
Koval Andrii <kovala@edi.su>
2017-10-21 00:24:59 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import subprocess
 import sys
 
-def getNumberEnv(command):
-    listEnv = subprocess.check_output(command).decode("utf-8").splitlines()
-    return len(listEnv)
+def getEnv(command):
+    listEnv = subprocess.check_output(command).decode("utf-8").rsplit()
+    return listEnv
 
 os=sys.platform
-#print(sys.platform)
 
 if os=='linux':
-    print('Congratulation you have linux platform and you environment have ', getNumberEnv('printenv'), ' lines')
-
-if os=='windows':
-    print('Ahtung!!! BIG BROTHER you have windows platform and you environment have ', getNumberEnv('printenv'), ' lines')
+    print('Congratulation you have a linux platform and your environment have', len(getEnv('printenv')), 'lines')
+    print(getEnv('printenv'))
+elif os=='win32' or os=='win64':
+    print('Ahtung!!! BIG BROTHER you have a windows platform and your environment have', len(getEnv('c:\Windows\system32\cmd.exe /C set')), 'lines')
+    print(getEnv('c:\Windows\system32\cmd.exe /C set'))
+else:
+    print('Your system unknown.')
